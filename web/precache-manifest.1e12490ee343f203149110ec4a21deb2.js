5c195d9f64ab54950e69160056c4d64c0c346d82
Stephen Pope <stephengpope@gmail.com>
2019-10-04 07:53:42 -0700

    added web files

---
+++ web/precache-manifest.1e12490ee343f203149110ec4a21deb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94eb50dc5220ad4e59efb1f13cd5911c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03b847408ca9a6323303",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "07837049a8838c390f17",
+    "url": "/static/js/2.d814b34e.chunk.js"
+  },
+  {
+    "revision": "03b847408ca9a6323303",
+    "url": "/static/js/main.2cfeafca.chunk.js"
+  },
+  {
+    "revision": "7f45875b99a5ef02142c",
+    "url": "/static/js/runtime-main.c2763ce9.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
